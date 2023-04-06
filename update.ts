2c4639d5ccb377c473ab9a648681ff7432ac0846
Siddharth Sharma <75090218+sidhelpbot@users.noreply.github.com>
2023-04-06 22:50:05 +0530

    update update.js

--- update.ts
+++ update.ts
@@ -6,17 +6,38 @@ import {closeServer} from "./keep_alive"
 let update = async (bot: any, client: any, e: any) => {
 let y: any = new Chk(client, e)
   
-    y.edit('Importing GitHub repository...');
+   let idd = await y.edit('Importing GitHub repository...', {d: false});
     exec("cd ./ && git fetch --all && git reset --hard origin/main", async (error: any, stdout: any, stderr: any) => {
       if (error) {
-        y.edit(`Error GitHub repository: ${error.message} \nPlease try one time more`);
+        y.edit(`Error GitHub repository: ${error.message} \nPlease try one time more`, {id: idd.id});
         return;
       }
       if (stderr) {
-        y.edit(`STDError updating GitHub repository: ${stderr} \nPlease try one time more`);
-        return;
+        await y.edit(`Wait while importing new code...`, {d: false, id: idd.id});
+        return await exec("cd ./ && git fetch --all && git reset --hard origin/main", async (er: any, sout: any, sr: any) => {
+        if(er)
+          return y.edit("unable to import git try one time more");
+         else {
+             await sleep(5000)
+             await y.edit('Imported new updates Successfully\n\nNow restarting server please wait...', {d: false, id: idd.id});
+             
+           
+         exec('./node_modules/.bin/pm2 restart 0', (r: any, ut: any, st: any) => {
+        if (r) 
+          return y.edit(`Error restarting Replit instance: ${r.message}`, {id: idd.id});
+        
+        if (st) 
+          return y.edit(`Error restarting Replit instance: ${st}`, {idd.id});
+        
+        y.edit('Successfully updated: Restarted bot', {id: idd.id});
+      })
+           
+         }
+          
+        })
+        
       }
-      y.edit('Imported new updates Successfully\nNow restarting server please wait...');
+      y.edit('Imported new updates Successfully\nNow restarting server please wait...', {d: false, id: idd.id});
       // await client.destroy();
       // await sleep(60000)
       
@@ -28,19 +49,18 @@ let y: any = new Chk(client, e)
       await sleep(5000)
       console.log("yes")
       exec('./node_modules/.bin/pm2 restart 0', (error: any, stdout: any, stderr: any) => {
-        if (error) {
-          y.edit(`Error restarting Replit instance: ${error.message}`);
-          return;
-        }
-        if (stderr) {
-          y.edit(`Error restarting Replit instance: ${stderr}`);
-          return;
-        }
+        if (error) 
+          return y.edit(`Error restarting Replit instance: ${error.message}`, {id: idd.id});
+        
+        if (stderr) 
+          return y.edit(`Error restarting Replit instance: ${stderr}`);
+        
         y.edit('Successfully updated: Restarted bot');
-      });
+      })
     });
 }
-function sleep(ms: any) {
+
+async function sleep(ms: any) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
