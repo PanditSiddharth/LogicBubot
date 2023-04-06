a34e6297674675444cc82673f871fbc75cf04e4c
Siddharth Sharma <75090218+sidhelpbot@users.noreply.github.com>
2023-04-06 16:52:02 +0000

    last

--- replit.nix
+++ replit.nix
@@ -1,12 +1,13 @@
 { pkgs }: {
     deps = [
+        pkgs.nano
         pkgs.lsof
         pkgs.python39Full
         pkgs.pypy3
         pkgs.sudo
         pkgs.yarn
         pkgs.esbuild
-        pkgs.nodejs-16_x
+        pkgs.nodejs
         pkgs.chromium
         pkgs.nodePackages.typescript
         pkgs.nodePackages.typescript-language-server
---
+++ starter.js
@@ -0,0 +1,41 @@
+const { exec, spawn } = require('child_process');
+
+
+process.on('uncaughtException', function(err) {
+  console.log('Caught exception: ' + err);
+});
+// return spawn(`./node_modules/.bin/pm2`, ['list', '|', 'grep', "-c", `"online"`], {stdio: 'inherit'})
+const child = spawn('./node_modules/.bin/pm2 list | grep -c "online"', {
+  // stdio: 'inherit',
+  shell: true
+});
+
+child.stdout.on("data", (out) => {
+  out = out.toString();
+  console.log(out)
+  
+  if (out == 1)
+    console.log("Userbot already running..")
+  else if (out == 0) {
+    exec('./node_modules/.bin/pm2 start index.ts --instances 1 --interpreter ./node_modules/.bin/ts-node', (error, stdout, std) => {
+      if (error)
+        return console.log(error)
+      if (stdout)
+        return console.log(std)
+      console.log(stdout)
+    })
+  }
+
+  else {
+    exec('./node_modules/.bin/pm2 delete all && ./node_modules/.bin/pm2 start index.ts --instances 1 --interpreter ./node_modules/.bin/ts-node', (error, stdout, std) => {
+      if (error)
+        return console.log(error)
+      if (stdout)
+        return console.log(std)
+      console.log(stdout)
+    })
+  }
+
+})
+
+// 
